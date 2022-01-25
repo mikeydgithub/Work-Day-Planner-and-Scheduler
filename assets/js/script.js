73e1ab117f3d0d8246343761e06df700ccfa8f58
Michael Diamond <94988620+mikeydgithub@users.noreply.github.com>
2022-01-25 13:58:52 -0500

    setup interval function to display current time

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var currentDay = document.querySelector("#currentDay");
+
+// creating a continous clock for currentDay
+setInterval(function(){
+    var now = moment();
+    var timeReadable = now.format("MMM DD, YYYY - hh:mm:ss a");
+    currentDay.textContent = timeReadable;
+}, 1000);
+
