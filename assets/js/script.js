e179c9805d162b72aefdb23ad22c6589f6b797d1
werdflows <werdsflows@gmail.com>
2022-01-26 20:43:55 -0500

    Add task update

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,14 +9,29 @@ setInterval(function(){
     currentDay.textContent = timeReadable;
 }, 1000);
 
-function echoInput(task_btn){
-    // get task input
+function enterText(){
+
+}
+
+function addTask(task_btn){
+    //Get task input
     let task_input = task_btn.previousElementSibling;
-    //read and log task from input box
-    console.log(task_input.value)
-        
+    let task_value = task_input.value;
 
+    //Get task list
+    let task_card = task_btn.closest(".card"); 
+    let task_list = task_card.querySelector("ul");
 
+    //Create list item for task
+    let task_item = document.createElement("li");
+    console.log(task_item);
+    task_item.appendChild(document.createTextNode(task_value));
+    console.log(task_item);
+    task_list.appendChild(task_item);
+    console.log(task_list);
+    //Read and log task from input box
+    console.log(task_value);
+}
 
 // setup rows to have enterable text
 //listToDo.addEventListener('click', enterText);
